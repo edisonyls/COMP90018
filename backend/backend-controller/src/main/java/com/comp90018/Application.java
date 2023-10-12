990502e86799fafac8a953dd1862f042bcd7fd79
Xingju Nie <tivvvvffffq@gmail.com>
2023-10-13 09:26:00 +1100

    application.yml

--- backend/backend-controller/src/main/java/com/comp90018/Application.java
+++ backend/backend-controller/src/main/java/com/comp90018/Application.java
@@ -2,8 +2,10 @@ package com.comp90018;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
+@MapperScan(basePackages = "com.comp90018.mapper")
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
