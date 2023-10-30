2d6ecc06e16119b0b4f68776ba0a11e57b43957f
xueeettt <xuetingz1@student.unimelb.edu.au>
2023-10-30 17:03:28 +1100

    Post upload without pic

---
+++ backend/backend-model/src/main/java/com/comp90018/bo/UploadPostBO.java
@@ -0,0 +1,36 @@
+package com.comp90018.bo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.validation.constraints.NotBlank;
+
+@Data
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class UploadPostBO {
+
+    private String userId;
+
+    private String postType;
+
+    private String title;
+
+    private String description;
+
+    private Double latitude;
+
+    private Double longitude;
+
+    private String petCategory;
+
+    private String petBread;
+
+    private String petName;
+
+    private String contactNumber;
+
+}
---
+++ backend/backend-service/src/main/java/com/comp90018/impl/PostServiceImpl.java
@@ -0,0 +1,64 @@
+package com.comp90018.impl;
+
+import com.comp90018.idworker.Sid;
+import com.comp90018.mapper.PostMapper;
+import com.comp90018.pojo.Post;
+import com.comp90018.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PostServiceImpl implements PostService {
+
+    @Autowired
+    private PostMapper postMapper;
+
+    @Autowired
+    private Sid sid;
+
+    @Override
+    public Post createPetMissingPost(String userId, int postType, String petCategory, String petBread, String petName, Double latitude, Double longitude) {
+        Post post = new Post();
+        String postId = sid.nextShort();
+        post.setId(postId);
+        post.setPosterId(userId);
+        post.setPostType(postType);
+        post.setPetCategory(petCategory);
+        post.setPetBread(petBread);
+        post.setPetName(petName);
+        post.setLatitude(latitude);
+        post.setLongitude(longitude);
+        return null;
+    }
+
+    @Override
+    public Post createFoundAPetPost(String userId, int postType, String petCategory, String petBread, String petName, String contactNumber, String title, String description, Double latitude, Double longitude) {
+        Post post = new Post();
+        String postId = sid.nextShort();
+        post.setId(postId);
+        post.setPosterId(userId);
+        post.setPostType(postType);
+        post.setPetCategory(petCategory);
+        post.setPetBread(petBread);
+        post.setPetName(petName);
+        post.setContactNum(contactNumber);
+        post.setTitle(title);
+        post.setDescription(description);
+        post.setLatitude(latitude);
+        post.setLongitude(longitude);
+        return null;
+    }
+
+    @Override
+    public Post createGeneralPost(String userId, String title, String description, Double latitude, Double longitude) {
+        Post post = new Post();
+        String postId = sid.nextShort();
+        post.setId(postId);
+        post.setPosterId(userId);
+        post.setTitle(title);
+        post.setDescription(description);
+        post.setLatitude(latitude);
+        post.setLongitude(longitude);
+        return null;
+    }
+}
