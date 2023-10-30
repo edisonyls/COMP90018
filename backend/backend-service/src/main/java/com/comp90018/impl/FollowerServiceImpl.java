0f8ec197271183ed5b6b045005ee676abccfab62
tivvvvffffq <tivvvvffffq@gmail.com>
2023-10-30 19:00:43 +1100

    follow service

---
+++ backend/backend-service/src/main/java/com/comp90018/impl/FollowerServiceImpl.java
@@ -0,0 +1,56 @@
+package com.comp90018.impl;
+
+import com.comp90018.enums.FriendEnum;
+import com.comp90018.idworker.Sid;
+import com.comp90018.mapper.FollowersMapper;
+import com.comp90018.pojo.Followers;
+import com.comp90018.service.FollowerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+import tk.mybatis.mapper.entity.Example;
+
+import java.util.List;
+
+public class FollowerServiceImpl implements FollowerService {
+    @Autowired
+    private Sid sid;
+
+    @Autowired
+    private FollowersMapper followersMapper;
+
+    @Transactional
+    @Override
+    public void doFollow(String followerId, String followingId) {
+        Followers follower = new Followers();
+        String id = sid.nextShort();
+
+        follower.setId(id);
+        follower.setFollowerId(followerId);
+        follower.setFollowingId(followingId);
+
+        //the sequence of following and follower should be inverse
+        Followers following = queryIsFollower(followingId, followerId);
+        if(following != null) {
+            following.setIsFollowerFriendOfMine(FriendEnum.YES.getFriend());
+            followersMapper.updateByPrimaryKey(following);
+
+            follower.setIsFollowerFriendOfMine(FriendEnum.YES.getFriend());
+        }else {
+            follower.setIsFollowerFriendOfMine(FriendEnum.NO.getFriend());
+        }
+        followersMapper.insert(follower);
+    }
+
+    public Followers queryIsFollower(String followerId, String followingId) {
+        Example example = new Example(Followers.class);
+        Example.Criteria criteria = example.createCriteria().andEqualTo("followingId", followingId).andEqualTo("followerId", followerId);
+        List<Followers> followers = followersMapper.selectByExample(example);
+
+        if(followers != null && !followers.isEmpty()) {
+            return followers.get(0);
+        }
+
+        return null;
+    }
+
+}
---
+++ backend/backend-service/src/main/java/com/comp90018/service/FollowerService.java
@@ -0,0 +1,6 @@
+package com.comp90018.service;
+
+public interface FollowerService {
+
+    public void doFollow(String followerId, String followingId);
+}
