1f4c8e1aafd2e46cc3c08cbe5834d718ff173c07
tivvvvffffq <tivvvvffffq@gmail.com>
2023-11-04 20:30:46 +1100

    message service update

--- backend/backend-mapper/src/main/java/com/comp90018/mongoDao/MessageDao.java
+++ backend/backend-mapper/src/main/java/com/comp90018/mongoDao/MessageDao.java
@@ -4,6 +4,9 @@ import com.comp90018.dto.Message;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface MessageDao extends MongoRepository<Message, String> {
+    List<Message> findAllBySenderIdOrReceiverIdOrderByTimeDesc(String senderId, String receiverId);
 }
--- backend/backend-service/src/main/java/com/comp90018/impl/MessageServiceImpl.java
+++ backend/backend-service/src/main/java/com/comp90018/impl/MessageServiceImpl.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
+import java.util.List;
 import java.util.Map;
 
 @Service
@@ -25,5 +26,15 @@ public class MessageServiceImpl implements MessageService {
                 receiverId, receiver.getNickname(), receiver.getProfile(),
                 type, content, new Date()));
 
+    }
+
+    @Override
+    public List<Message> listAllMessage(String receiverId) {
+        return null;
+    }
+
+    @Override
+    public List<Message> listMessageWith1User() {
+        return null;
     }
 }
