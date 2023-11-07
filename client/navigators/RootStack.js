c7b88864459991fbc5508392c66c083a4bbee8be
HAINING SUN <hainingsun@192-168-1-107.tpgi.com.au>
2023-11-07 23:32:29 +1100

    fix navigater problem

--- client/navigators/RootStack.js
+++ client/navigators/RootStack.js
@@ -15,9 +15,9 @@ import FindLostPet from "../screens/FindLostPet";
 import NotificationScreen from "../screens/NotificationScreen";
 import SecurityScreen from "../screens/SecurityScreen";
 import ContactUsScreen from "../screens/ContactUsScreen";
-
 import MessageScreen from "../screens/MessageScreen";
 import ActivitiesScreen from "../screens/ActivitiesScreen";
+import PostScreen from "../screens/PostScreen";
 
 
 const Stack = createNativeStackNavigator();
