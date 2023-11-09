f014f879f5639e1243aefb60c1c83aeb3bd2e485
Agent007Doris <140674941+Agent007Doris@users.noreply.github.com>
2023-11-10 05:48:27 +1100

    add map user direction sensor

--- client/screens/MapScreen.js
+++ client/screens/MapScreen.js
@@ -17,11 +17,15 @@ import * as Location from "expo-location";
 import { Animated, Easing } from "react-native";
 import { useUserContext } from "../context/userContext";
 import { getAllPosts, queryUserInfo } from '../api/auth';
-import { EvilIcons } from '@expo/vector-icons';
+// import { EvilIcons } from '@expo/vector-icons';
+import { Magnetometer } from 'expo-sensors';
+import { Entypo } from '@expo/vector-icons';
+
 
 const RadarAnimation = React.memo(() => {
   const scaleValue = new Animated.Value(0); // 初始值为0
   const [selectedMenu, setSelectedMenu] = useState("All");
+  
 
 
   const animateRadar = () => {
@@ -33,6 +37,7 @@ const RadarAnimation = React.memo(() => {
       useNativeDriver: true, // 使用原生动画驱动
     }).start(() => animateRadar()); // 动画结束后重新开始，创建循环效果
   };
+  
 
   useEffect(() => {
     animateRadar(); // 开始动画
@@ -77,7 +82,7 @@ const MapScreen = () => {
   const [posts, setPosts] = useState([]);
   const [userInfo, setUserInfo] = useState(null);
   const [allPosts, setAllPosts] = useState([]); // 存储从后端获取的所有帖子
-
+  const [direction, setDirection] = useState(null);
   const userLocation = location
     ? {
         latitude: location.coords.latitude,
@@ -105,6 +110,30 @@ const MapScreen = () => {
       headerShown: false,
     });
   }, [navigation]);
+
+  useEffect(() => {
+    // 订阅磁力计事件
+    Magnetometer.setUpdateInterval(1000);  // 设置更新频率为1000毫秒
+    const subscription = Magnetometer.addListener((data) => {
+      // 使用磁力计数据来计算方向
+      let angle = Math.atan2(data.y, data.x);
+      if (angle >= 0) {
+        angle = angle * (180 / Math.PI);
+      } else {
+        angle = (angle + 2 * Math.PI) * (180 / Math.PI);
+      }
+      // 将角度调整180度
+      angle = (angle + 180) % 360;
+      // 将调整后的角度转换为度数并设置方向
+      setDirection(angle);
+    });
+  
+    return () => {
+      // 清理订阅
+      subscription.remove();
+    };
+  }, []);
+
   useEffect(() => {
     (async () => {
       setIsLoading(true); // 开始加载数据
@@ -135,7 +164,7 @@ const MapScreen = () => {
 
     fetchAllPosts();
   })();
-  }, []);
+}, []);
 
   
 
@@ -244,10 +273,14 @@ const MapScreen = () => {
             {userLocation && (
               <Marker coordinate={userLocation}>
                 <View
-                  style={{ alignItems: "center", justifyContent: "center" }}
+                 style={{
+                  alignItems: "center",
+                  justifyContent: "center",
+                  transform: [{ rotate: `${direction}deg` }], // 根据设备方向旋转标记
+                }}
                 >
                   <RadarAnimation />
-                  <EvilIcons name="location" size={40} color="#9747ff" />
+                  <Entypo name="direction" size={40} color="#9747ff" />
                 </View>
               </Marker>
             )}
